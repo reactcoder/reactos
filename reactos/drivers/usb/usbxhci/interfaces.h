12d93110c0e3cf72a2f857b503e883b73d5d132a
reactcoder <reactcoder@gmail.com>
2017-03-08 22:45:29 +0200

    [XHCI] Add interface functions.
    Add SetRuntimeRegister, GetRuntimeRegister, SetOperationalRegister,
    CompleteCommandRequest, CompleteTransferRequest, IsEventRingEmpty and
    GetEventRingDequeuePointer interface functions.

--- reactos/drivers/usb/usbxhci/interfaces.h
+++ reactos/drivers/usb/usbxhci/interfaces.h
@@ -4,9 +4,25 @@
 //
 // IUSBHardwareDevice
 //
-#define DEFINE_ABSTRACT_USBXHCIHARDWARE()
+#define DEFINE_ABSTRACT_USBXHCIHARDWARE()               \
+    STDMETHOD_(VOID, SetRuntimeRegister)(THIS_          \
+        IN ULONG Offset,                                \
+        IN ULONG Value) PURE;                           \
+    STDMETHOD_(ULONG, GetRuntimeRegister)(THIS_         \
+        IN ULONG Offset) PURE;                          \
+    STDMETHOD_(VOID, SetOperationalRegister)(THIS_      \
+        IN ULONG Offset,                                \
+        IN ULONG Value) PURE;
 
-#define IMP_IUSBXHCIHARDWARE
+#define IMP_IUSBXHCIHARDWARE                            \
+    STDMETHODIMP_(VOID) SetRuntimeRegister(             \
+        IN ULONG Offset,                                \
+        IN ULONG Value);                                \
+    STDMETHODIMP_(ULONG) GetRuntimeRegister(            \
+        IN ULONG Offset);                               \
+    STDMETHODIMP_(VOID) SetOperationalRegister(         \
+        IN ULONG Offset,                                \
+        IN ULONG Value);
 
 DECLARE_INTERFACE_(IXHCIHardwareDevice, IUSBHardwareDevice)
 {
@@ -36,9 +52,21 @@ typedef IXHCIRequest *PXHCIREQUEST;
 //
 // IUSBQueue
 //
-#define DEFINE_ABSTRACT_USBXHCIQUEUE()
+#define DEFINE_ABSTRACT_USBXHCIQUEUE()                      \
+    STDMETHOD_(VOID, CompleteCommandRequest)(THIS_          \
+        IN PTRB TransferRequestBlock) PURE;                 \
+    STDMETHOD_(VOID, CompleteTransferRequest)(THIS_         \
+        IN PTRB TransferRequestBlock) PURE;                 \
+    STDMETHOD_(BOOLEAN, IsEventRingEmpty)(THIS_) PURE;      \
+    STDMETHOD_(PTRB, GetEventRingDequeuePointer)(THIS_) PURE;
 
-#define IMP_IXHCIQUEUE
+#define IMP_IXHCIQUEUE                                      \
+    STDMETHODIMP_(VOID) CompleteCommandRequest(             \
+        IN PTRB TransferRequestBlock);                      \
+    STDMETHODIMP_(VOID) CompleteTransferRequest(            \
+        IN PTRB TransferRequestBlock);                      \
+    STDMETHODIMP_(BOOLEAN) IsEventRingEmpty(VOID);          \
+    STDMETHODIMP_(PTRB) GetEventRingDequeuePointer(VOID);
 
 DECLARE_INTERFACE_(IXHCIQueue, IUSBQueue)
 {
